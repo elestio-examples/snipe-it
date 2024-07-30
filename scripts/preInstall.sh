7bef575b8b2dcee100b978cfbd1740f9ba2ef8be
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-07-30 09:33:22 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,10 +1,9 @@
 #set env vars
-#set -o allexport; source .env; set +o allexport;
+set -o allexport; source .env; set +o allexport;
+
+APP_KEY=$(yes | docker-compose run -T --rm app php artisan key:generate --show)
 
 cat << EOT >> ./.env
 
-APP_KEY=
+APP_KEY=${APP_KEY}
 EOT
-
-
-yes | docker-compose run -T --rm app php artisan key:generate
